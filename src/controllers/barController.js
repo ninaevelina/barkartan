b69386dab7e88d5fff948dde958af44df8f4c142
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-03-02 09:45:21 +0100

    minor changes to controllers
    Co-authored-by: Lisa Lindblom <lisalindblom@users.noreply.github.com>
    Co-authored-by: juliacarlberg <juliacarlberg@users.noreply.github.com>

--- src/controllers/barController.js
+++ src/controllers/barController.js
@@ -1,23 +1,21 @@
-//get all bars
-exports.getAllBars = async(req,res) {
+const { sequelize } = require("../database/config");
+const { bars } = require("../data/bar");
+const { user } = require("../data/user");
+const { userRoles } = require("../constants/users");
+const { UnauthorizedError } = require("../utils/errors");
+const { QueryTypes } = require("sequelize");
 
-};
+//get all bars
+exports.getAllBars = async (req, res) => {};
 
 //get bar by id
-exports.getBarById = async(req,res) {
+exports.getBarById = async (req, res) => {};
 
-};
 //get bar by city_id
-exports.getBarByCityId = async(req,res){
-
-};
+exports.getBarByCityId = async (req, res) => {};
 
 //create new bar
-exports.createNewBar = async(req,res){
-
-};
+exports.createNewBar = async (req, res) => {};
 
 //delete bar
-exports.deleteBarById = async(req,res){
-
-};
+exports.deleteBarById = async (req, res) => {};
--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -1,4 +1,11 @@
+const { sequelize } = require("../database/config");
+const { userRoles } = require("../constants/users");
+const { QueryTypes } = require("sequelize");
+const { UnauthorizedError, NotFoundError } = require("../utils/errors");
+
 // get all reviews
+
+exports.getAllReviews = async (req, res) => {};
 
 // get review by id
 
