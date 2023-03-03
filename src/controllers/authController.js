ece0c909d0b2b73365c9a107e93c848d34b26248
Julia Carlberg <juliacarlbrg@gmail.com>
2023-03-03 12:36:12 +0100

    login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -25,6 +25,7 @@ exports.register = async (req, res) => {
 			'INSERT INTO user (username, email, password, role) VALUES ($username, $email, $password, "admin")', 
 			{
 				bind: {
+          username: username,
 					password: hashedpassword,
 					email: email
 				}
@@ -36,6 +37,7 @@ exports.register = async (req, res) => {
 			'INSERT INTO user (username, email, password) VALUES ($username, $email, $password)', 
 			{
 				bind: {
+          username: username,
 					password: hashedpassword,
 					email: email,
 				},
@@ -53,8 +55,6 @@ exports.login = async (req, res) => {
   // Place candidate email and password into local variables
   const { email, password: canditatePassword } = req.body;
 
-  // Check if user with that email exits in db
-  // prettier-ignore
   const [user, metadata] = await sequelize.query(
 		'SELECT * FROM user WHERE email = $email LIMIT 1;', {
 		bind: { email },
@@ -65,28 +65,21 @@ exports.login = async (req, res) => {
 
   if (!user) throw new UnauthenticatedError("Invalid Credentials");
 
-  // Check if password is correct
-  // @ts-ignore
   const isPasswordCorrect = await bcrypt.compare(
     canditatePassword,
     user.password
   );
   if (!isPasswordCorrect) throw new UnauthenticatedError("Invalid Credentials");
 
-  // Create JWT payload (aka JWT contents)
   const jwtPayload = {
-    // @ts-ignore
     userId: user.id,
-    // @ts-ignore
     email: user.email,
-    role: user["is_admin"] === 1 ? userRoles.ADMIN : userRoles.USER,
+    role: user["is_admin"] === 1 ? userRoles.ADMIN : userRoles.USER
   };
 
-  // Create the JWT token
   const jwtToken = jwt.sign(jwtPayload, process.env.JWT_SECRET, {
     expiresIn: "1h" /* 1d */,
   });
 
-  // Return the token
   return res.json({ token: jwtToken, user: jwtPayload });
 };
