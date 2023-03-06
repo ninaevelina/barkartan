2b5e9bbae11df66d831a9318721439628e3b91fc
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-03-06 11:27:41 +0100

    added validation for review

--- src/middleware/validation/validationSchemas.js
+++ src/middleware/validation/validationSchemas.js
@@ -22,3 +22,18 @@ exports.loginSchema = [
   body("email").isEmail().withMessage("You must provide a valid email address"),
   body("password").not().isEmpty().withMessage("You must provide a password"),
 ];
+
+exports.reviewSchema = [
+  body("review_text")
+    .not()
+    .isEmpty()
+    .isLength({ min: 2 })
+    .withMessage("Your review must contain at least 2 characters"),
+  body("rating")
+    .not()
+    .isEmpty()
+    .isInt({ min: 1, max: 5 })
+    .withMessage(
+      "You must enter your review rating, please provide your rating between 1 and 5"
+    ),
+];
