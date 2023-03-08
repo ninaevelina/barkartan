4417e647d28abff3eeb57336e4fe0b67d675b342
lisamanssonlindblom <113437951+lisamanssonlindblom@users.noreply.github.com>
2023-03-08 13:58:15 +0100

    req.user funkar

--- src/middleware/authenticationMiddleware.js
+++ src/middleware/authenticationMiddleware.js
@@ -15,12 +15,17 @@ exports.isAuthenticated = async (req, res, next) => {
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("payload auth", payload);
 
     req.user = {
+      //@ts-ignore
       userId: payload.userId,
-      role: payload.role,
+      //@ts-ignore
       username: payload.username,
+      //@ts-ignore
+      is_admin: payload.is_admin,
     };
+    console.log("userId", req.user.id, req.user.username, payload);
     next();
   } catch (error) {
     throw new UnauthenticatedError("Authentication invalid");
@@ -28,7 +33,6 @@ exports.isAuthenticated = async (req, res, next) => {
 };
 exports.authorizeRoles = (...roles) => {
   return (req, res, next) => {
-    //if (!req.user?.is_admin || !roles.includes(req.user.is_admin)) {
     if (!req.user.is_admin === 1) {
       throw new UnauthorizedError("Unauthorized Access");
     }
